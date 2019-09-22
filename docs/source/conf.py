c1e5de51502d5b1c233df5f91b72bde435b613a6
Kevin Pounot <kpounot@hotmail.fr>
2019-09-22 13:42:27 +0200

    Updating docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../../package'))
 sys.path.insert(0, os.path.abspath('../../package/nPDyn'))
 sys.path.insert(0, os.path.abspath('../../package/nPDyn/dataManipulation'))
 sys.path.insert(0, os.path.abspath('../../package/nPDyn/dataParsers'))
